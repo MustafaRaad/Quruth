b0a0c3d2bb415e43afaf4552628fe285a95d62a7
Mustafa Raad <mustafa.raad.m7@gmail.com>
2022-04-19 22:31:13 +0300

    loan types for guest

--- resources/views/guest-category/guest-car.blade.php
+++ resources/views/guest-category/guest-car.blade.php
@@ -4,19 +4,9 @@
         <div class="mt-3 text-center w-100 py-3 bg-white rounded-sm shadow-md">
             Loans application and management system
         </div>
-        @if (Route::has('login'))
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            @auth
-            <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-            @else
             <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-            @endif
-            @endauth
         </div>
-        @endif
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
